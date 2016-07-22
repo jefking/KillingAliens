2a2cbe8a2c58027c390a8c2066259301eebae786
Jef King <jefkin@microsoft.com>
2016-07-22 14:11:31 -0700

    grid == good !== undefined

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+var boardHeight = 50;
+var boardWidth = 50;
+
 function start() {
     var board = document.getElementById('board');
     var alien = document.createElement('div');
@@ -6,23 +9,35 @@ function start() {
     var tower = document.createElement('div');
     tower.className = 'tower sprite';
     board.appendChild(tower);
+
+    initBoard();
+}
+
+function initBoard()
+{
+    var board = document.getElementById('board');
+    board.innerHTML = '';
+
+    for (i = 0; i < (boardHeight * boardWidth); i++) {
+        var point = document.createElement('div');
+        point.className = 'point sprite';
+        board.appendChild(point);
+    }
+
     layout();
 }
 
 function layout() {
     var board = document.getElementById('board');
-    board.innerHTML = '';
-
-    var max = 10;
-    var rowHeight = window.innerHeight / max;
-    var colHeight = window.innerWidth / max;
+    
+    var current = 0;
+    
+    var rowHeight = window.innerHeight / boardHeight;
+    var colHeight = window.innerWidth / boardWidth;
     for (i = rowHeight / 2; i < window.innerHeight; i+=rowHeight) {
-        for (j = colHeight / 2; j <  window.innerWidth; j+=colHeight) {
-            var point = document.createElement('div');
-            point.className = 'point sprite';
-            point.style.top = i + 'px';
-            point.style.left = j + 'px';
-            board.appendChild(point);
+        for (j = colHeight / 2; j <  window.innerWidth; j+=colHeight, current++) {
+            board.childNodes[current].style.top = i + 'px';
+            board.childNodes[current].style.left = j + 'px';
         }
     }
 }
