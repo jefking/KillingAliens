81de686fda1de94affe4bd23824305149be0354b
Jef King <jefkin@microsoft.com>
2016-07-22 13:28:03 -0700

    w00t w00t

--- app.js
+++ app.js
@@ -5,11 +5,20 @@ function start()
 
 function layout()
 {
-    var board = window.getElementByName('board');
-    var max = 4;
-    for (i = 0; i < max; i++)
-    {
-        board.addChild(point);
-        window.screen.width / (i + 1);
-    }
+    var board = document.getElementById('board');
+    console.log(board);
+    var point = board.createElement('div');
+    point.className = 'point sprite';
+    // var tower = board.createElement('<div class="tower sprite"></div>');
+    // var sprite = board.createElement('<div class="sprite sprite"></div>');
+    board.appendChild(point);
+    // board.appendChild(tower);
+    // board.appendChild(sprite);
+
+    // var max = 4;
+    // for (i = 0; i < max; i++)
+    // {
+    //     board.addChild(point);
+    //     window.screen.width / (i + 1);
+    // }
 }
